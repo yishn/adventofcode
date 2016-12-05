9361e8f55d7aa1a3365ec33838f62694a74f31b1
Yichuan Shen <shen.yichuan@gmail.com>
2016-12-05 17:48:50 +0100

    Make password decoding more fun

--- 2016/5.js
+++ 2016/5.js
@@ -2,21 +2,27 @@ const crypto = require('crypto')
 
 let input = 'abbhdwsy'
 
+const hexDigits = '0123456789abcdef'
+
+let randomDigit = () => hexDigits[Math.floor(Math.random() * hexDigits.length)]
 let md5 = str => crypto.createHash('md5').update(str).digest('hex')
-let printCode = code => code.reduce((s, x) => s + (x != null ? x : '_'), '')
+let printCode = code => process.stdout.write('\r' + code.map(x => x || randomDigit()).join(''))
 
 function solve(length, predicate, update) {
     let code = Array.apply(null, {length})
     let index = 0
 
+    printCode(code)
+
     while (code.filter(x => x != null).length < code.length) {
         let str = input + index
         let hash = md5(str)
 
-        if (predicate(hash)) {
+        if (predicate(hash))
             code = update(code, hash)
-            console.log(printCode(code))
-        }
+
+        if (index % 10000 == 0)
+            printCode(code)
 
         index++
     }
@@ -27,10 +33,9 @@ function solve(length, predicate, update) {
 let interesting = hash => hash.slice(0, 5).split('').every(x => x == 0)
 let update5 = (code, hash) => (code[code.findIndex(x => x == null)] = hash[5], code)
 
-console.log('Part 1:\t' + solve(8, interesting, update5))
+console.log('\rPart 1:\t' + solve(8, interesting, update5))
 
 let valid = length => hash => interesting(hash) && !isNaN(hash[5]) && +hash[5] < length
 let update6 = (code, hash) => (code[hash[5]] = code[hash[5]] || hash[6], code)
 
-console.log()
-console.log('Part 2:\t' + solve(8, valid(8), update6))
+console.log('\rPart 2:\t' + solve(8, valid(8), update6))
