3c95c3762bbc0a8d23c8d22ecab16dd06e656d52
Yichuan Shen <shen.yichuan@gmail.com>
2016-12-08 10:53:19 +0100

    Add day 8 solution

---
+++ 2016/8.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+
+let input = fs.readFileSync('./input8.txt', 'utf8').trim()
+let instructions = input.split('\n')
+    .map(x => x.match(/(rect|rotate (row|column)) ([xy]=)?(\d+)( by |x)(\d+)/))
+    .map(([, type, , , x, , y]) => [type, +x, +y])
+
+let printScreen = screen => console.log(screen.map(row => row.map(v => v ? '#' : '.').join('')).join('\n'))
+let newArray = length => Array.apply(null, {length})
+
+let shiftArray = (a, s) => [...a.slice(-s % a.length), ...a.slice(0, -s % a.length)]
+let countOn = screen => !isNaN(screen) ? +screen : screen.reduce((sum, x) => sum + countOn(x), 0)
+
+let rotateRow = (screen, y, s) => {
+    screen[y] = shiftArray(screen[y], s)
+
+    return screen
+}
+
+let rotateColumn = (screen, x, s) => {
+    let column = screen.map(row => row[x])
+    let shifted = shiftArray(column, s)
+    screen.forEach((row, i) => row[x] = shifted[i])
+
+    return screen
+}
+
+let drawRect = (screen, x, y) => {
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
+            screen[j][i] = true
+        }
+    }
+
+    return screen
+}
+
+function getScreen(instructions, width, height) {
+    return instructions.reduce((screen, [type, x, y]) => {
+        let dict = {
+            'rect': drawRect,
+            'rotate column': rotateColumn,
+            'rotate row': rotateRow
+        }
+
+        return dict[type](screen, x, y)
+    }, newArray(height).map(x => newArray(width).map(y => false)))
+}
+
+let screen = getScreen(instructions, 50, 6)
+console.log('Part 1:\t' + countOn(screen))
+console.log('Part 2:')
+printScreen(screen)
