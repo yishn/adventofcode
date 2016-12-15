3111e947dfc865eb54a9d7938338673be03f6692
Yichuan Shen <shen.yichuan@gmail.com>
2016-12-15 11:05:49 +0100

    Remove mod function

--- 2016/15.js
+++ 2016/15.js
@@ -5,15 +5,13 @@ let data = input.split('\n')
     .map(line => line.match(/#\d+\D+(\d+)\D+0\D+(\d+)/))
     .map(([, m, start]) => [+m, +start])
 
-let mod = (a, m) => (a % m + m) % m
-
 function solveChineseRemainder(image, mods) {
     if (image.length != mods.length) return null
 
     let product = mods.reduce((p, x) => p * x)
 
     for (let i = 0; i < product; i++) {
-        if (mods.every((m, j) => mod(i - image[j], m) == 0)) {
+        if (mods.every((m, j) => (i - image[j]) % m == 0)) {
             return i
         }
     }
