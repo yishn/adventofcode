564d7a09cb388dc380196d299443b3ea9afcd104
Yichuan Shen <shen.yichuan@gmail.com>
2016-12-19 21:04:01 +0100

    Add part 2 of day 19

--- 2016/19.js
+++ 2016/19.js
@@ -7,38 +7,32 @@ let printProgress = p => process.stdout.write(p < 1 ? '\r[' + [
     Array(20 - Math.round(p * 20)).fill(' ')
 ].map(x => x.join('')).join('>') + `] ${Math.round(p * 100)}% ` : '\r' + Array(30).fill(' ').join('') + '\r')
 
-function stealPresent(index, steal) {
-    if (!state[index]) return
-
-    state[steal] = false
-    participants--
-}
-
 function leftNeighborElf(index) {
-    let i = (index + 1) % state.length
+    let i = index + 1
+    if (i == state.length) i = 0
 
-    while (!state[i])
-        i = (i + 1) % state.length
+    while (!state[i]) {
+        i++
+        if (i == state.length) i = 0
+    }
 
-    return i == index ? null : i
+    return i
 }
 
-function playGame(nextIndex) {
+function playGame1() {
     let i = 0
-    let lastPercent = 0
 
     printProgress(0)
 
     while (participants > 1) {
-        let percent = 1 - participants / state.length
-
-        if (percent - lastPercent >= 0.05) {
+        if (participants % 100000 == 0) {
+            let percent = 1 - participants / state.length
             printProgress(percent)
-            lastPercent = percent
         }
 
-        let n = nextIndex(i)
-        stealPresent(i, n)
+        state[leftNeighborElf(i)] = false
+        participants--
+
         i = leftNeighborElf(i)
     }
 
@@ -46,4 +40,59 @@ function playGame(nextIndex) {
     return state.findIndex(x => x)
 }
 
-console.log('Part 1:\t' + (playGame(leftNeighborElf) + 1))
+console.log('Part 1:\t' + (playGame1() + 1))
+
+state = Array(input).fill(1)
+participants = state.length
+
+function acrossElf(index) {
+    let skip = participants / 2
+    let i = index
+
+    while (skip-- >= 1) {
+        i = leftNeighborElf(i)
+    }
+
+    return i
+}
+
+function playGame2() {
+    let i = 0
+
+    printProgress(0)
+
+    while (participants > 1) {
+        let index = i
+        let across = acrossElf(i)
+        let j = across
+
+        let counter = 1
+        let limit = participants / 2
+
+        while (true) {
+            if (participants % 100000 == 0) {
+                let percent = 1 - participants / state.length
+                printProgress(percent)
+            }
+
+            state[j] = false
+
+            i = leftNeighborElf(i)
+
+            if (participants % 2 == 1) {
+                j = leftNeighborElf(leftNeighborElf(j))
+            } else {
+                j = leftNeighborElf(j)
+            }
+
+            participants--
+
+            if (counter++ >= limit) break
+        }
+    }
+
+    printProgress(1)
+    return state.findIndex(x => x)
+}
+
+console.log('Part 2:\t' + (playGame2() + 1))
