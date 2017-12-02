e7477b991a2f452d25ac35475e8adecceb7fac85
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-02 11:27:07 +0100

    Use body.innerText instead of textContent

--- 2017/01.js
+++ 2017/01.js
@@ -1,5 +1,5 @@
 // Part 1
-[...document.body.textContent.trim()].reduce((s,c,i,a)=>(c==a[(i+1)%a.length])*c+s,0)
+[...document.body.innerText.trim()].reduce((s,c,i,a)=>(c==a[(i+1)%a.length])*c+s,0)
 
 // Part 2
-[...document.body.textContent.trim()].reduce((s,c,i,a)=>(l=>c==a[(i+l/2)%l])(a.length)*c+s,0)
+[...document.body.innerText.trim()].reduce((s,c,i,a)=>(l=>c==a[(i+l/2)%l])(a.length)*c+s,0)
