5025dff25ad01906d40751338a458d69085d5264
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-04 00:30:58 +0100

    Even shorter solutions

--- 2017/03.js
+++ 2017/03.js
@@ -1,5 +1,5 @@
 // Part 1
-I=document.querySelector('.puzzle-input').innerText,w=~~Math.sqrt(I),w+=w%2,x=y=0,[...Array((w+1)**2-I)].map((_,i)=>(x+=i<w?1:i>=2*w&i<3*w?-1:0,y+=i>=3*w?-1:i>=w&i<2*w?1:0)),Math.abs(w/2-x)+Math.abs(w/2-y)
+w=~~Math.sqrt(I),w+=w%2;for(I=document.querySelector('.puzzle-input').innerText,x=y=i=0;i<(w+1)**2-I;i++){x+=i<w?1:i>=2*w&i<3*w?-1:0;y+=i>=3*w?-1:i>=w&i<2*w?1:0}Math.abs(w/2-x)+Math.abs(w/2-y)
 
 // Part 2
-I=document.querySelector('.puzzle-input').innerText,S={'0,0':1},x=y=0,[...Array(~~Math.sqrt(I))].map((_,w)=>w>1&w%2<1&&[...Array(4*w)].map((_,i)=>S[[x,y]]<=I&&(x+=!i|i>=3*w?1:i>=w&i<2*w?-1:0,y+=i>0&i<w?-1:i>=2*w&i<3*w?1:0,S[[x,y]]=[-1,0,1].reduce((s,d,_,a)=>a.reduce((s,e)=>s+(S[[x+d,y+e]]||0),s),0)))),S[[x,y]]
+for(I=document.querySelector('.puzzle-input').innerText,S={'0,0':1},x=y=w=0;w<I;w+=2){for(i=0;i<4*w&S[[x,y]]<=I;i++){x+=!i|i>=3*w?1:i>=w&i<2*w?-1:0;y+=i>0&i<w?-1:i>=2*w&i<3*w?1:0;S[[x,y]]=[-1,0,1].reduce((s,d,_,a)=>a.reduce((s,e)=>s+(S[[x+d,y+e]]||0),s),0)}}S[[x,y]]
--- README.md
+++ README.md
@@ -4,4 +4,3 @@ My solutions to Advent of Code
 ## 2017 Challenge
 - Code should be pasteable into dev tools console
 - Code should be as short as possible
-- Code consists of one expression
