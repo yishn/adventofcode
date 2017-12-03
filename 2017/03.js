10fa014781194f21e57e111f241a69f2748bbe3b
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-03 23:55:04 +0100

    Add day 3 part 2 solution

--- 2017/03.js
+++ 2017/03.js
@@ -1,2 +1,5 @@
 // Part 1
-I=+document.querySelector('.puzzle-input').innerText,w=Array(I).findIndex((_,i)=>i%2>0&i**2>I)-1,x=y=0,[...Array((w+1)**2-I)].map((_,i)=>(x+=i<w?1:i>=2*w&i<3*w?-1:0,y+=i>=3*w?-1:i>=w&i<2*w?1:0)),Math.abs(w/2-x)+Math.abs(w/2-y)
+I=document.querySelector('.puzzle-input').innerText,w=~~Math.sqrt(I),w+=w%2,x=y=0,[...Array((w+1)**2-I)].map((_,i)=>(x+=i<w?1:i>=2*w&i<3*w?-1:0,y+=i>=3*w?-1:i>=w&i<2*w?1:0)),Math.abs(w/2-x)+Math.abs(w/2-y)
+
+// Part 2
+I=document.querySelector('.puzzle-input').innerText,S={'0,0':1},x=y=0,[...Array(~~Math.sqrt(I))].map((_,w)=>w>1&w%2<1&&[...Array(4*w)].map((_,i)=>S[[x,y]]<=I&&(x+=!i|i>=3*w?1:i>=w&i<2*w?-1:0,y+=i>0&i<w?-1:i>=2*w&i<3*w?1:0,S[[x,y]]=[-1,0,1].reduce((s,d,_,a)=>a.reduce((s,e)=>s+(S[[x+d,y+e]]||0),s),0)))),S[[x,y]]
