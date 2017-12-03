e11ba9408792b3cd5643772725014492c84447bf
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-03 22:44:19 +0100

    Shorter part 1 version

--- 2017/03.js
+++ 2017/03.js
@@ -1,3 +1,2 @@
 // Part 1
-
-I=document.querySelector('.puzzle-input').innerText,w=Array(+I).findIndex((_,i)=>i%2>0&i**2>I)-1,x=0,y=0,[...Array(4*w)].map((_,i)=>(d=(w+1)**2-i>I,x+=d*(i<w?1:i>=2*w&i<3*w?-1:0),y+=d*(i>=3*w?-1:i>=w&i<2*w?1:0))),Math.abs(w/2-x)+Math.abs(w/2-y)
+I=+document.querySelector('.puzzle-input').innerText,w=Array(I).findIndex((_,i)=>i%2>0&i**2>I)-1,x=y=0,[...Array((w+1)**2-I)].map((_,i)=>(x+=i<w?1:i>=2*w&i<3*w?-1:0,y+=i>=3*w?-1:i>=w&i<2*w?1:0)),Math.abs(w/2-x)+Math.abs(w/2-y)
