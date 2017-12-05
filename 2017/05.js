3352cc30b56b9b38c70bf07ca2e232e6a7898ce4
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-05 20:07:33 +0100

    Add day 5 solution

---
+++ 2017/05.js
@@ -0,0 +1,5 @@
+// Part 1
+for(s=p=0,a=document.body.innerText.split`\n`;p<a.length-1&p>=0;s++)p+=++a[p]-1;s
+
+// Part 2
+for(s=p=0,a=document.body.innerText.split`\n`;p<a.length-1&p>=0;s++){q=p;p+=+a[p];a[q]-=a[q]>2?1:-1}s
