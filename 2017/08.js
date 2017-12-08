82c96e1f0f2e935fb7199452551655bc068e30ed
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-08 19:41:41 +0100

    Add day 8 solution part 2

--- 2017/08.js
+++ 2017/08.js
@@ -1,2 +1,5 @@
 // Part 1
 R={},document.body.innerText.split`\n`.map(l=>l&&([x,a,y,,z,o,v]=l.split` `,R[x]=(R[x]|0)+y*(a[0]=='i'?1:-1)*eval((R[z]|0)+o+v))),Math.max(...Object.values(R))
+
+// Part 2
+m=0,R={},document.body.innerText.split`\n`.map(l=>l&&([x,a,y,,z,o,v]=l.split` `,m=Math.max(m,R[x]=(R[x]|0)+y*(a[0]=='i'?1:-1)*eval((R[z]|0)+o+v)))),m
