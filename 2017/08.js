b13408e83e364622766bf4574c962966884c9754
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-08 19:39:08 +0100

    Much shorter day 8 part 1 solution

--- 2017/08.js
+++ 2017/08.js
@@ -1,2 +1,2 @@
 // Part 1
-for(p=0,R={},I=document.body.innerText.split`\n`.map(x=>x.split` `),c=a=>eval(a.join``);p<I.length;p++)if(c([R[I[p][4]]|0,...I[p].slice(-2)])){[x,o,y]=I[p];R[x]=(R[x]|0)+y*(o[0]=='i'?1:-1)}Math.max(...Object.values(R))
+R={},document.body.innerText.split`\n`.map(l=>l&&([x,a,y,,z,o,v]=l.split` `,R[x]=(R[x]|0)+y*(a[0]=='i'?1:-1)*eval((R[z]|0)+o+v))),Math.max(...Object.values(R))
