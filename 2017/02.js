0cb30b0f30a4e69dd3a788b8460ad7a933e8b43e
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-09 13:05:47 +0100

    Use | instead of ||

--- 2017/02.js
+++ 2017/02.js
@@ -2,4 +2,4 @@
 document.body.innerText.split`\n`.map(l=>(n=l.split`\t`,Math.max(...n)-Math.min(...n))).reduce((s,x)=>s+x)
 
 // Part 2
-document.body.innerText.split`\n`.map(l=>l.split`\t`.map((x,_,n)=>n.find(y=>y>+x&y%x<1)/x).find(x=>!isNaN(x))||0).reduce((s,x)=>s+x)
+document.body.innerText.split`\n`.map(l=>l.split`\t`.map((x,_,n)=>n.find(y=>y>+x&y%x<1)/x).find(x=>!isNaN(x))|0).reduce((s,x)=>s+x)
--- 2017/03.js
+++ 2017/03.js
@@ -2,4 +2,4 @@
 I=document.querySelector('.puzzle-input').innerText,w=~~Math.sqrt(I),w+=w%2;for(x=y=i=0;i<(w+1)**2-I;i++){x+=i<w?1:i>=2*w&i<3*w?-1:0;y+=i>=3*w?-1:i>=w&i<2*w?1:0}Math.abs(w/2-x)+Math.abs(w/2-y)
 
 // Part 2
-for(I=document.querySelector('.puzzle-input').innerText,S={'0,0':1},x=y=w=0;w<I;w+=2){for(i=0;i<4*w&S[[x,y]]<=I;i++){x+=!i|i>=3*w?1:i>=w&i<2*w?-1:0;y+=i>0&i<w?-1:i>=2*w&i<3*w?1:0;S[[x,y]]=[-1,0,1].reduce((s,d,_,a)=>a.reduce((s,e)=>s+(S[[x+d,y+e]]||0),s),0)}}S[[x,y]]
+for(I=document.querySelector('.puzzle-input').innerText,S={'0,0':1},x=y=w=0;w<I;w+=2){for(i=0;i<4*w&S[[x,y]]<=I;i++){x+=!i|i>=3*w?1:i>=w&i<2*w?-1:0;y+=i>0&i<w?-1:i>=2*w&i<3*w?1:0;S[[x,y]]=[-1,0,1].reduce((s,d,_,a)=>a.reduce((s,e)=>s+(S[[x+d,y+e]]|0),s),0)}}S[[x,y]]
