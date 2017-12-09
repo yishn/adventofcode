f5b810951050e9c66be8452e960bd4b29600d4c1
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-09 13:08:32 +0100

    Shorter code by reversing position of ++

--- 2017/05.js
+++ 2017/05.js
@@ -1,5 +1,5 @@
 // Part 1
-for(s=p=0,a=document.body.innerText.split`\n`;p<a.length-1&p>=0;s++)p+=++a[p]-1;s
+for(s=p=0,a=document.body.innerText.split`\n`;p<a.length-1&p>=0;s++)p+=a[p]++;s
 
 // Part 2
 for(s=p=0,a=document.body.innerText.split`\n`;p<a.length-1&p>=0;s++){q=p;p+=+a[p];a[q]-=a[q]>2?1:-1}s
