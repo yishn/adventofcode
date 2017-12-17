45a62243f3d59073076151b3fa8981bf19a5c822
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-17 19:44:47 +0100

    Shorter day 17 part 2 solution

--- 2017/17.js
+++ 2017/17.js
@@ -2,4 +2,4 @@
 for(p=0,C=[0],i=1;i<2018;){p=(+document.querySelector('.puzzle-input').innerText+p)%i;C.splice(++p,0,i++)}C[++p%--i]
 
 // Part 2
-for(s=+document.querySelector('.puzzle-input').innerText,r=z=p=0,i=1;i<=5*10**7;i++){p=(s+p)%i+1;p<=z?z++:p==z+1&&(r=i)}r
+for(s=+document.querySelector('.puzzle-input').innerText,r=z=p=i=0;i<5*10**7;){p=(s+p)%++i+1;p<=z?z++:p==z+1&&(r=i)}r
