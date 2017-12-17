e3982638f36b4e5feef2e0e3454e9738eb0e99dd
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-17 19:50:38 +0100

    One char shorter solution for day 6 solutions

--- 2017/06.js
+++ 2017/06.js
@@ -1,5 +1,5 @@
 // Part 1
-for(c=0,s=document.body.innerText.split`\t`.map(x=>+x),M={};!M[s];){M[s]=c++;for(i=s.indexOf(Math.max(...s)),n=s[i],s[i]=0;n>0;n--)s[++i%s.length]++}c
+for(c=0,s=document.body.innerText.split`\t`.map(x=>+x),M={};!M[s];){M[s]=c++;for(i=s.indexOf(Math.max(...s)),n=s[i],s[i]=0;n-->0;)s[++i%s.length]++}c
 
 // Part 2
-for(c=0,s=document.body.innerText.split`\t`.map(x=>+x),M={};!M[s];){M[s]=c++;for(i=s.indexOf(Math.max(...s)),n=s[i],s[i]=0;n>0;n--)s[++i%s.length]++}c-M[s]
+for(c=0,s=document.body.innerText.split`\t`.map(x=>+x),M={};!M[s];){M[s]=c++;for(i=s.indexOf(Math.max(...s)),n=s[i],s[i]=0;n-->0;)s[++i%s.length]++}c-M[s]
