f4a16b873752f14668e83d0f5fb420fbfad650b4
Yichuan Shen <shen.yichuan@gmail.com>
2017-12-17 10:58:27 +0100

    Add day 17 solution

---
+++ 2017/17.js
@@ -0,0 +1,5 @@
+// Part 1
+for(p=0,C=[0],i=1;i<2018;){p=(+document.querySelector('.puzzle-input').innerText+p)%i;C.splice(++p,0,i++)}C[++p%--i]
+
+// Part 2
+for(s=+document.querySelector('.puzzle-input').innerText,r=z=p=0,i=1;i<=5*10**7;i++){p=(s+p)%i+1;p<=z?z++:p==z+1&&(r=i)}r
