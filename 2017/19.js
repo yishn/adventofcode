67ba69b5869107087e587f4fa805d68f839c3cbe
Yichuan Shen <yichuan.shen@sap.com>
2017-12-19 15:02:25 +0100

    Add day 19 part 2 solution

--- 2017/19.js
+++ 2017/19.js
@@ -1,2 +1,5 @@
 // Part 1
-for(C=r='',M=document.body.innerText.split`\n`.map(x=>[...x]),x=M[0].indexOf('|'),y=e=1,d=0,c=(x,y)=>M[y]&&M[y][x];C!=' ';){for(;!'+ '.includes(C=c(x,y));){r+=!'|-'.includes(C)?C:'';x+=d;y+=e}d=d!=0?0:c(x+1,y)!=' '?1:-1;e=e!=0?0:c(x,y+1)!=' '?1:-1;x+=d;y+=e}r
+for(C=r='',M=document.body.innerText.split`\n`.map(x=>[...x]),x=M[0].indexOf('|'),y=e=1,d=0,c=(x,y)=>M[y]&&M[y][x];C!=' ';){for(;!'+ '.includes(C=c(x,y));){r+=C.replace(/\||-/g,'');x+=d;y+=e}d=d!=0?0:c(x+1,y)!=' '?1:-1;e=e!=0?0:c(x,y+1)!=' '?1:-1;x+=d;y+=e}r
+
+// Part 2
+for(C='',M=document.body.innerText.split`\n`.map(x=>[...x]),x=M[0].indexOf('|'),y=e=1,r=d=0,c=(x,y)=>M[y]&&M[y][x];C!=' ';){for(;!'+ '.includes(C=c(x,y));){r++;x+=d;y+=e}d=d!=0?0:c(x+1,y)!=' '?1:-1;e=e!=0?0:c(x,y+1)!=' '?1:-1;r++;x+=d;y+=e}r
