ddd8f491acd22256046ef0a41b657e83b9c4e8b1
Yichuan Shen <shen.yichuan@gmail.com>
2018-12-18 01:43:47 +0100

    Add 2018 day 17 draft

--- 2018/17.rs
+++ 2018/17.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::{HashSet, HashMap};
@@ -10,7 +11,7 @@ fn get_input() -> std::io::Result<String> {
     Ok(contents)
 }
 
-type Point = (usize, usize);
+type Point = (isize, isize);
 type DepthMap = HashSet<Point>;
 
 fn parse(input: &str) -> DepthMap {
@@ -47,14 +48,122 @@ fn parse(input: &str) -> DepthMap {
             .unwrap_or((0, 0));
 
         (xstart..xend + 1)
-        .flat_map(move |x| (ystart..yend + 1).map(move |y| (x, y)))
+        .flat_map(move |x| (ystart..yend + 1).map(move |y| (x as isize, y as isize)))
     })
     .collect()
+}
+
+fn fill_water(source: Point, map: &DepthMap) -> (DepthMap, DepthMap) {
+    let miny = map.iter().cloned().map(|(_, y)| y).min().unwrap_or(source.1);
+    let maxy = map.iter().cloned().map(|(_, y)| y).max().unwrap_or(source.1);
+
+    let mut water = DepthMap::new();
+    let mut flows = DepthMap::new();
+    let mut blocked = map.clone();
+
+    fn flows_through((x, y): Point, blocked: &DepthMap) -> bool {
+        !blocked.contains(&(x, y + 1))
+    }
+
+    fn inner(
+        source: Point,
+        map: &DepthMap,
+        water: &mut DepthMap,
+        flows: &mut DepthMap,
+        blocked: &mut DepthMap,
+        miny: isize,
+        maxy: isize
+    ) {
+        let (sx, sy) = source;
+
+        let (ax, ay) = (sy..maxy + 1)
+            .map(|y| (sx, y))
+            .find(|&p| !flows_through(p, &blocked))
+            .unwrap_or((sx, maxy));
+
+        let ((lx, ly), (rx, ry)) = {
+            let left = (0..)
+                .map(|x| (ax - x, ay))
+                .find(|&(x, y)| {
+                    blocked.contains(&(x - 1, y))
+                    || flows_through((x, y), &blocked)
+                })
+                .unwrap();
+
+            let right = (0..)
+                .map(|x| (ax + x, ay))
+                .find(|&(x, y)| {
+                    blocked.contains(&(x + 1, y))
+                    || flows_through((x, y), &blocked)
+                })
+                .unwrap();
+
+            (left, right)
+        };
+
+        // Update flows & water
+
+        let left_overflow = flows_through((lx, ly), &blocked);
+        let right_overflow = flows_through((rx, ry), &blocked);
+        let overflow = left_overflow || right_overflow;
+        let top = (lx..rx + 1).map(|x| (x, ay)).collect::<DepthMap>();
+
+        let old_water_count = water.len();
+        let old_flow_count = flows.len();
+
+        println!("{:?} {:?} {:?}: {:?}, {:?}, {:?} - {}, {}", source, left_overflow, right_overflow, (lx, ly), (ax, ay), (rx, ry), water.len(), flows.len());
+
+        if !overflow {
+            *water = water.union(&top).cloned().collect();
+            *blocked = blocked.union(&top).cloned().collect();
+        } else {
+            *flows = flows
+                .union(&top)
+                .cloned()
+                .chain(
+                    if !flows.contains(&(sx, sy + 1)) {
+                        (max(sy, miny)..ay + 1)
+                    } else {
+                        (0..0)
+                    }
+                    .map(|y| (sx, y))
+                )
+                .collect();
+        }
+
+        // Propagate
+
+        let (left_source, right_source) = (
+            left_overflow && (lx, ly) != (ax, ay),
+            right_overflow && (rx, ry) != (ax, ay)
+        );
+
+        if left_source {
+            inner((lx, ly), map, water, flows, blocked, miny, maxy);
+        }
+
+        if right_source {
+            inner((rx, ry), map, water, flows, blocked, miny, maxy);
+        }
+
+        if overflow && old_flow_count == flows.len() && old_water_count == water.len() {
+            return;
+        }
+
+        if !overflow {
+            inner(source, map, water, flows, blocked, miny, maxy);
+        }
+    }
+
+    inner(source, map, &mut water, &mut flows, &mut blocked, miny, maxy);
+
+    (water, flows)
 }
 
 fn main() {
     let input = get_input().unwrap();
     let depth_map = parse(&input);
+    let (water, flows) = fill_water((500, 0), &depth_map);
 
-    println!("{:?}", depth_map);
+    println!("Part 1: {}", water.union(&flows).count());
 }
