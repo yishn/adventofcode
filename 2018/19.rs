0842aa27dcaf247070fde297d71dc5fa0f3a57ab
Yichuan Shen <shen.yichuan@gmail.com>
2018-12-19 14:18:07 +0100

    Add naive solution to 2018 day 19

---
+++ 2018/19.rs
@@ -0,0 +1,100 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::{HashSet, HashMap};
+
+fn get_input() -> std::io::Result<String> {
+    let mut file = File::open("19.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(contents)
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct State(Vec<usize>);
+
+impl State {
+    fn new(first_register: usize) -> State {
+        State(vec![first_register, 0, 0, 0, 0, 0])
+    }
+
+    fn get(&self, r: usize) -> usize {
+        self.0.get(r).cloned().unwrap_or(0)
+    }
+
+    fn set(&mut self, r: usize, v: usize) {
+        self.0.get_mut(r).map(|x| *x = v);
+    }
+
+    fn op(&mut self, op: Operation) {
+        let v = match op.0 {
+            "addr" => self.get(op.1) + self.get(op.2),
+            "addi" => self.get(op.1) + op.2,
+            "mulr" => self.get(op.1) * self.get(op.2),
+            "muli" => self.get(op.1) * op.2,
+            "banr" => self.get(op.1) & self.get(op.2),
+            "bani" => self.get(op.1) & op.2,
+            "borr" => self.get(op.1) | self.get(op.2),
+            "bori" => self.get(op.1) | op.2,
+            "setr" => self.get(op.1),
+            "seti" => op.1,
+            "gtir" => if op.1 > self.get(op.2) { 1 } else { 0 },
+            "gtri" => if self.get(op.1) > op.2 { 1 } else { 0 },
+            "gtrr" => if self.get(op.1) > self.get(op.2) { 1 } else { 0 },
+            "eqir" => if op.1 == self.get(op.2) { 1 } else { 0 },
+            "eqri" => if self.get(op.1) == op.2 { 1 } else { 0 },
+            "eqrr" => if self.get(op.1) == self.get(op.2) { 1 } else { 0 },
+            _ => return
+        };
+
+        self.set(op.3, v);
+    }
+}
+
+type Operation<'a> = (&'a str, usize, usize, usize);
+
+fn parse(input: &str) -> (usize, Vec<Operation>) {
+    let mut ip_register = 0;
+    let program = input.lines()
+        .filter_map(|line| {
+            if line.starts_with("#ip ") {
+                line[4..].parse::<usize>().ok()
+                .map(|x| ip_register = x);
+            }
+
+            let mut tokens = line.split(' ');
+            let op_type = tokens.next();
+
+            op_type.map(|op_type| (
+                op_type,
+                tokens
+                    .filter_map(|x| x.parse::<usize>().ok())
+                    .collect::<Vec<_>>()
+            ))
+        })
+        .filter(|(_, v)| v.len() == 3)
+        .map(|(op_type, v)| (op_type, v[0], v[1], v[2]))
+        .collect();
+
+    (ip_register, program)
+}
+
+fn run_program(mut state: State, ip_register: usize, program: &[Operation]) -> State {
+    while let Some(&operation) = program.get(state.0[ip_register]) {
+        state.op(operation);
+        state.0[ip_register] += 1;
+    }
+
+    state
+}
+
+fn main() {
+    let input = get_input().unwrap();
+    let (ip_register, program) = parse(&input);
+
+    let state = run_program(State::new(0), ip_register, &program);
+    println!("Part 1: {}", state.0[0]);
+
+    let state = run_program(State::new(1), ip_register, &program);
+    println!("Part 2: {}", state.0[0]);
+}
