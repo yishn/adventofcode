9d01d1276d0dd4331d6560b7f76ee80c570649a4
Yichuan Shen <shen.yichuan@gmail.com>
2019-12-02 19:56:38 +0100

    Add 2019 day 2 solution

---
+++ 2019/02.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn get_input() -> std::io::Result<String> {
+  let mut file = File::open("02.txt")?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+
+  Ok(contents)
+}
+
+fn run_program(program: &mut Vec<usize>) -> &mut Vec<usize> {
+  let mut pointer = 0;
+
+  loop {
+    let v = |p| program[program[p]];
+    let value = match program[pointer] {
+      1 => v(pointer + 1) + v(pointer + 2),
+      2 => v(pointer + 1) * v(pointer + 2),
+      99 => break,
+      _ => panic!()
+    };
+
+    let target_index = program[pointer + 3];
+    program[target_index] = value;
+
+    pointer += 4;
+  }
+
+  program
+}
+
+fn calculate_output(mut program: Vec<usize>, input: (usize, usize)) -> usize {
+  program[1] = input.0;
+  program[2] = input.1;
+
+  run_program(&mut program);
+
+  program[0]
+}
+
+fn main() {
+  let input = get_input().unwrap();
+  let program = input.split(',')
+    .filter_map(|x| x.trim().parse::<usize>().ok())
+    .collect::<Vec<_>>();
+
+  println!("Part 1: {}", calculate_output(program.clone(), (12, 2)));
+
+  for i in 0..100 {
+    for j in 0..100 {
+      let output = calculate_output(program.clone(), (i, j));
+
+      if output == 19690720 {
+        println!("Part 2: {}", i * 100 + j);
+        return;
+      }
+    }
+  }
+}
