7e6793a8fc1a44bed7f902166acb05b9d9ddc5a1
Yichuan Shen <yichuan.shen@sap.com>
2019-12-06 13:22:10 +0100

    Use of generics instead of lifetimes

--- 2019/06.rs
+++ 2019/06.rs
@@ -1,16 +1,17 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::Hash;
 
 #[derive(Debug)]
-struct OrbitMap<'a> {
-  objects: HashSet<&'a str>,
-  parents: HashMap<&'a str, &'a str>,
-  children: HashMap<&'a str, Vec<&'a str>>
+struct OrbitMap<T: Hash + Eq> {
+  objects: HashSet<T>,
+  parents: HashMap<T, T>,
+  children: HashMap<T, Vec<T>>
 }
 
-impl<'a> OrbitMap<'a> {
-  fn new() -> OrbitMap<'a> {
+impl<T: Copy + Hash + Eq> OrbitMap<T> {
+  fn new() -> OrbitMap<T> {
     OrbitMap {
       objects: HashSet::new(),
       parents: HashMap::new(),
@@ -18,12 +19,12 @@ impl<'a> OrbitMap<'a> {
     }
   }
 
-  fn insert(&mut self, parent: &'a str, child: &'a str) {
+  fn insert(&mut self, parent: T, child: T) {
     self.objects.insert(parent);
     self.objects.insert(child);
     self.parents.insert(child, parent);
 
-    if let Some(children) = self.children.get_mut(parent) {
+    if let Some(children) = self.children.get_mut(&parent) {
       children.push(child);
     } else {
       self.children.insert(parent, vec![child]);
@@ -39,7 +40,7 @@ fn get_input() -> std::io::Result<String> {
   Ok(contents)
 }
 
-fn parse_input<'a>(input: &'a str) -> OrbitMap<'a> {
+fn parse_input(input: &str) -> OrbitMap<&str> {
   input.lines()
   .filter_map(|line| {
     let mut ids = line.split(')');
@@ -55,11 +56,11 @@ fn parse_input<'a>(input: &'a str) -> OrbitMap<'a> {
   })
 }
 
-fn get_ancestors<'a>(orbit_map: &'a OrbitMap, object: &'a str) -> Vec<&'a str> {
+fn get_orbits<T: Copy + Hash + Eq>(orbit_map: &OrbitMap<T>, object: T) -> Vec<T> {
   let mut result = vec![];
   let mut object = object;
 
-  while let Some(&parent) = orbit_map.parents.get(object) {
+  while let Some(&parent) = orbit_map.parents.get(&object) {
     result.push(parent);
     object = parent;
   }
@@ -67,7 +68,7 @@ fn get_ancestors<'a>(orbit_map: &'a OrbitMap, object: &'a str) -> Vec<&'a str> {
   result
 }
 
-fn bfs<'a>(orbit_map: &'a OrbitMap, start: &'a str, target: &'a str) -> Option<Vec<&'a str>> {
+fn bfs<T: Copy + Hash + Eq>(orbit_map: &OrbitMap<T>, start: T, target: T) -> Option<Vec<T>> {
   let mut previous_map = HashMap::new();
   previous_map.insert(start, None);
 
@@ -79,15 +80,15 @@ fn bfs<'a>(orbit_map: &'a OrbitMap, start: &'a str, target: &'a str) -> Option<V
       break;
     }
 
-    let parent_list = [orbit_map.parents.get(current)];
-    let next_list = orbit_map.children.get(current)
+    let parent_list = [orbit_map.parents.get(&current)];
+    let next_list = orbit_map.children.get(&current)
       .map(|children| children.iter().chain(
         parent_list.into_iter().filter_map(|&x| x)
       ));
 
     if let Some(next_list) = next_list {
       for &next in next_list {
-        if !previous_map.contains_key(next) {
+        if !previous_map.contains_key(&next) {
           previous_map.insert(next, Some(current));
           queue.push_back(next);
         }
@@ -95,7 +96,7 @@ fn bfs<'a>(orbit_map: &'a OrbitMap, start: &'a str, target: &'a str) -> Option<V
     }
   }
 
-  if let None = previous_map.get(target) {
+  if let None = previous_map.get(&target) {
     return None;
   }
 
@@ -114,7 +115,7 @@ fn main() {
   let orbit_map = parse_input(&input);
 
   let total_ancestors = orbit_map.objects.iter()
-    .map(|&object| get_ancestors(&orbit_map, object).len())
+    .map(|&object| get_orbits(&orbit_map, object).len())
     .sum::<usize>();
 
   println!("Part 1: {}", total_ancestors);
