a10e310ab6d8bd82b166b040e71b2e33f2f3948b
Yichuan Shen <shen.yichuan@gmail.com>
2019-12-18 23:03:18 +0100

    Add day 18 part 1 solution

--- 2019/18.rs
+++ 2019/18.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
+use std::hash::Hash;
 
 #[derive(Debug, Copy, Clone)]
 enum Tile<T> {
@@ -51,9 +52,64 @@ fn parse_labyrinth(input: &str) -> (Labyrinth<char>, Option<Position>) {
   (labyrinth, entrance)
 }
 
+fn get_neighbors<T: Hash + Eq>(labyrinth: &Labyrinth<T>, (x, y): Position, keys: &Vec<T>) -> Vec<Position> {
+  [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)].into_iter()
+  .cloned()
+  .filter(|&(x, y)| x > 0 && y > 0)
+  .filter(|pos| match labyrinth.get(pos) {
+    Some(Tile::Passage) | Some(Tile::Key(_)) => true,
+    Some(Tile::Door(x)) => keys.contains(x),
+    _ => false
+  })
+  .collect()
+}
+
+fn get_all_keys(labyrinth: &Labyrinth<char>, position: Position, key_count: usize) -> Vec<Position> {
+  let mut queue = VecDeque::new();
+  let mut previous_map = HashMap::new();
+  let mut target = None;
+
+  queue.push_back((position, vec![]));
+  previous_map.insert((position, vec![]), None);
+
+  while let Some((position, keys)) = queue.pop_front() {
+    if keys.len() >= key_count {
+      target = Some((position, keys));
+      break;
+    }
+
+    for neighbor in get_neighbors(labyrinth, position, &keys) {
+      let mut new_keys = keys.clone();
+
+      if let Some(&Tile::Key(c)) = labyrinth.get(&neighbor) {
+        new_keys.push(c);
+        new_keys.sort();
+        new_keys.dedup();
+      }
+
+      let map_key = (neighbor, new_keys);
+
+      if !previous_map.contains_key(&map_key) {
+        queue.push_back((neighbor, map_key.1.clone()));
+        previous_map.insert(map_key, Some((position, keys.clone())));
+      }
+    }
+  }
+
+  let mut result = vec![target.unwrap()];
+
+  while let Some(Some(prev)) = previous_map.get(&result.last().unwrap()) {
+    result.push(prev.clone());
+  }
+
+  result.into_iter().rev().map(|(position, _)| position).collect()
+}
+
 fn main() {
   let input = get_input().unwrap();
-  let labyrinth = parse_labyrinth(&input);
+  let (labyrinth, position) = parse_labyrinth(&input);
 
-  println!("{:?}", labyrinth);
+  let path = get_all_keys(&labyrinth, position.unwrap(), 26);
+
+  println!("Part 1: {}", path.len() - 1);
 }
