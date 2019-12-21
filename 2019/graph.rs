1df591f9991862bb8f51cfcaae290cd5c0e88e87
Yichuan Shen <shen.yichuan@gmail.com>
2019-12-21 11:19:31 +0100

    Introduce Vertex trait

--- 2019/graph.rs
+++ 2019/graph.rs
@@ -7,28 +7,31 @@ enum NodeIterType {
   Dfs
 }
 
-pub struct PredecessorIter<'a, G: Graph<V>, V: Hash + Eq + Copy> {
+pub trait Vertex: Hash + Eq + Clone {}
+impl<T: Hash + Eq + Clone> Vertex for T {}
+
+pub struct PredecessorIter<'a, G: Graph<V>, V: Vertex> {
   node_iter: NodeIter<'a, G, V>
 }
 
-impl<'a, G: Graph<V>, V: Hash + Eq + Copy> PredecessorIter<'a, G, V> {
-  pub fn construct_path(&mut self, target: V) -> Option<Vec<V>> {
+impl<'a, G: Graph<V>, V: Vertex> PredecessorIter<'a, G, V> {
+  pub fn construct_path(self, target: V) -> Option<Vec<V>> {
     self.node_iter.construct_path(target)
   }
 }
 
-impl<'a, G: Graph<V>, V: Hash + Eq + Copy> Iterator for PredecessorIter<'a, G, V> {
+impl<'a, G: Graph<V>, V: Vertex> Iterator for PredecessorIter<'a, G, V> {
   type Item = (Option<V>, V);
 
   fn next(&mut self) -> Option<Self::Item> {
     self.node_iter.next()
     .and_then(|v| {
-      self.node_iter.predecessor_map.get(&v).map(|&p| (p, v))
+      self.node_iter.predecessor_map.get(&v).map(|p| (p.clone(), v))
     })
   }
 }
 
-pub struct NodeIter<'a, G: Graph<V>, V: Hash + Eq + Copy> {
+pub struct NodeIter<'a, G: Graph<V>, V: Vertex> {
   graph: &'a G,
   start: V,
   queue: VecDeque<V>,
@@ -36,21 +39,31 @@ pub struct NodeIter<'a, G: Graph<V>, V: Hash + Eq + Copy> {
   iter_type: NodeIterType
 }
 
-impl<'a, G: Graph<V>, V: Hash + Eq + Copy> NodeIter<'a, G, V> {
+impl<'a, G: Graph<V>, V: Vertex> NodeIter<'a, G, V> {
+  fn new(graph: &'a G, start: V, iter_type: NodeIterType) -> NodeIter<'a, G, V> {
+    NodeIter {
+      graph,
+      start: start.clone(),
+      queue: iter::once(start.clone()).collect(),
+      predecessor_map: iter::once((start, None)).collect(),
+      iter_type
+    }
+  }
+
   pub fn predecessor(self) -> PredecessorIter<'a, G, V> where Self: Sized {
     PredecessorIter {
       node_iter: self
     }
   }
 
-  pub fn construct_path(&mut self, target: V) -> Option<Vec<V>> {
+  pub fn construct_path(mut self, target: V) -> Option<Vec<V>> {
     if !self.predecessor_map.contains_key(&target) {
-      self.find(|&v| v == target);
+      self.find(|v| v == &target);
     }
 
     let mut path = vec![target];
 
-    while let Some(&Some(previous)) = self.predecessor_map.get(path.last().unwrap()) {
+    while let Some(Some(previous)) = self.predecessor_map.remove(path.last().unwrap()) {
       path.push(previous);
     }
 
@@ -64,7 +77,7 @@ impl<'a, G: Graph<V>, V: Hash + Eq + Copy> NodeIter<'a, G, V> {
   }
 }
 
-impl<'a, G: Graph<V>, V: Hash + Eq + Copy> Iterator for NodeIter<'a, G, V> {
+impl<'a, G: Graph<V>, V: Vertex> Iterator for NodeIter<'a, G, V> {
   type Item = V;
 
   fn next(&mut self) -> Option<Self::Item> {
@@ -74,13 +87,13 @@ impl<'a, G: Graph<V>, V: Hash + Eq + Copy> Iterator for NodeIter<'a, G, V> {
     };
 
     vertex.map(|vertex| {
-      for neighbor in self.graph.get_neighbors(vertex) {
+      for neighbor in self.graph.get_neighbors(vertex.clone()) {
         if self.predecessor_map.contains_key(&neighbor) {
           continue;
         }
 
-        self.queue.push_back(neighbor);
-        self.predecessor_map.insert(neighbor, Some(vertex));
+        self.queue.push_back(neighbor.clone());
+        self.predecessor_map.insert(neighbor, Some(vertex.clone()));
       }
 
       vertex
@@ -88,26 +101,14 @@ impl<'a, G: Graph<V>, V: Hash + Eq + Copy> Iterator for NodeIter<'a, G, V> {
   }
 }
 
-pub trait Graph<V: Hash + Eq + Copy> {
+pub trait Graph<V: Vertex> {
   fn get_neighbors(&self, vertex: V) -> Vec<V>;
 
   fn bfs<'a>(&'a self, start: V) -> NodeIter<'a, Self, V> where Self: Sized {
-    NodeIter {
-      graph: self,
-      start,
-      queue: iter::once(start).collect(),
-      predecessor_map: iter::once((start, None)).collect(),
-      iter_type: NodeIterType::Bfs
-    }
+    NodeIter::new(self, start, NodeIterType::Bfs)
   }
 
   fn dfs<'a>(&'a self, start: V) -> NodeIter<'a, Self, V> where Self: Sized {
-    NodeIter {
-      graph: self,
-      start,
-      queue: iter::once(start).collect(),
-      predecessor_map: iter::once((start, None)).collect(),
-      iter_type: NodeIterType::Dfs
-    }
+    NodeIter::new(self, start, NodeIterType::Dfs)
   }
 }
