94c4dc81887724f5312bca02ecc58d9a911fa518
Yichuan Shen <shen.yichuan@gmail.com>
2019-12-25 00:34:45 +0100

    Add day 18 part 2 solution

--- 2019/18.rs
+++ 2019/18.rs
@@ -3,7 +3,6 @@ use std::io::prelude::*;
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::rc::Rc;
-use std::fmt::Debug;
 
 mod graph;
 use graph::Graph;
@@ -18,7 +17,6 @@ enum Tile<K> {
 
 type Position = (usize, usize);
 type PositionKeys<K> = (Position, Rc<Vec<K>>);
-type PositionsKeys<K> = (Vec<Position>, Rc<Vec<K>>);
 type Labyrinth<K> = HashMap<Position, Tile<K>>;
 
 impl<K: Hash + Eq + Clone + Ord> Graph<PositionKeys<K>> for Labyrinth<K> {
@@ -45,25 +43,6 @@ impl<K: Hash + Eq + Clone + Ord> Graph<PositionKeys<K>> for Labyrinth<K> {
         }))
       },
       _ => None
-    })
-    .collect()
-  }
-}
-
-impl<K: Hash + Eq + Clone + Ord> Graph<PositionsKeys<K>> for Labyrinth<K> {
-  fn get_neighbors(&self, (positions, keys): PositionsKeys<K>) -> Vec<PositionsKeys<K>> {
-    positions.iter().cloned()
-    .map(|position| (position, keys.clone()))
-    .enumerate()
-    .flat_map(|(i, position_keys)| {
-      self.get_neighbors(position_keys).into_iter()
-      .map(move |x| (i, x))
-      .map(|(i, (position, keys))| {
-        let mut positions = positions.clone();
-        positions[i] = position;
-
-        (positions, keys)
-      })
     })
     .collect()
   }
@@ -107,9 +86,9 @@ fn parse_labyrinth(input: &str) -> (Labyrinth<char>, Option<Position>) {
   (labyrinth, entrance)
 }
 
-fn get_all_keys<K>(labyrinth: &Labyrinth<K>, position: Position, key_count: usize) -> Option<Vec<Position>>
+fn get_all_keys<K>(labyrinth: &Labyrinth<K>, position_key: PositionKeys<K>, key_count: usize) -> Option<Vec<Position>>
 where K: Hash + Eq + Clone + Ord {
-  let mut bfs_iter = labyrinth.bfs((position, Rc::new(vec![])));
+  let mut bfs_iter = labyrinth.bfs(position_key);
 
   bfs_iter
   .find(|(_, keys)| keys.len() == key_count)
@@ -123,32 +102,71 @@ where K: Hash + Eq + Clone + Ord {
   })
 }
 
-fn real_get_all_keys<K>(labyrinth: &Labyrinth<K>, positions: Vec<Position>, key_count: usize) -> Option<Vec<Vec<Position>>>
-where K: Hash + Eq + Clone + Ord + Debug {
-  let mut bfs_iter = labyrinth.bfs((positions, Rc::new(vec![])));
+fn get_size<K>(labyrinth: &Labyrinth<K>) -> (usize, usize) {
+  let size = labyrinth.keys().cloned()
+    .fold((None, None), |(max_x, max_y), (x, y)| {
+      (
+        [max_x, Some(x)].into_iter().filter_map(|&x| x).max(),
+        [max_y, Some(y)].into_iter().filter_map(|&y| y).max()
+      )
+    });
 
-  bfs_iter
-  .find(|(_, keys)| {
-    if keys.len() > 5 {
-      println!("{:?}", keys);
-    }
-    keys.len() == key_count
-  })
-  .and_then(|target| {
-    bfs_iter.construct_path(target)
-    .map(|path| {
-      path.into_iter()
-      .map(|(positions, _)| positions)
-      .collect()
+  match size {
+    (Some(x), Some(y)) => (x + 1, y + 1),
+    _ => (0, 0)
+  }
+}
+
+fn real_get_all_keys<K>(
+  labyrinth: &Labyrinth<K>,
+  (x, y): Position,
+  key_count: usize
+) -> (Option<Vec<Position>>, Option<Vec<Position>>, Option<Vec<Position>>, Option<Vec<Position>>)
+where K: Hash + Eq + Clone + Ord {
+  let (width, height) = get_size(labyrinth);
+  let positions = vec![(x - 1, y - 1), (x + 1, y - 1), (x - 1, y + 1), (x + 1, y + 1)];
+
+  let keys = labyrinth.iter()
+    .filter_map(|(&position, tile)| match tile {
+      Tile::Key(k) => Some((position, k.clone())),
+      _ => None
     })
-  })
+    .collect::<Vec<_>>();
+
+  let position_keys = positions.into_iter()
+    .enumerate()
+    .map(|(i, position)| {
+      let ignore_keys = keys.iter().cloned()
+        .filter(|&((x, y), _)| {
+          if i == 0 {
+            x > width / 2 || y > height / 2
+          } else if i == 1 {
+            x <= width / 2 || y > height / 2
+          } else if i == 2 {
+            x > width / 2 || y <= height / 2
+          } else if i == 3 {
+            x <= width / 2 || y <= height / 2
+          } else {
+            panic!()
+          }
+        })
+        .map(|(_, k)| k.clone())
+        .collect::<Vec<_>>();
+
+      (position, Rc::new(ignore_keys))
+    });
+
+  let mut results = position_keys
+    .filter_map(|position_key| get_all_keys(labyrinth, position_key, key_count));
+
+  (results.next(), results.next(), results.next(), results.next())
 }
 
 fn main() {
   let input = get_input().unwrap();
   let (mut labyrinth, position) = parse_labyrinth(&input);
 
-  let path = get_all_keys(&labyrinth, position.unwrap(), 26);
+  let path = get_all_keys(&labyrinth, (position.unwrap(), Rc::new(vec![])), 26);
   println!("Part 1: {}", path.unwrap().len() - 1);
 
   let (x, y) = position.unwrap();
@@ -158,8 +176,6 @@ fn main() {
   labyrinth.insert((x, y - 1), Tile::Wall);
   labyrinth.insert((x, y + 1), Tile::Wall);
 
-  let positions = vec![(x - 1, y - 1), (x + 1, y - 1), (x - 1, y + 1), (x + 1, y + 1)];
-  let path = real_get_all_keys(&labyrinth, positions, 26);
-
-  println!("Part 2: {}", path.unwrap().len() - 1);
+  let (p1, p2, p3, p4) = real_get_all_keys(&labyrinth, (x, y), 26);
+  println!("Part 2: {}", p1.unwrap().len() + p2.unwrap().len() + p3.unwrap().len() + p4.unwrap().len() - 4);
 }
